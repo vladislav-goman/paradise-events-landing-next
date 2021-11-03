4cb023e24b0442b6027af48fc4591cbe7e9c9073
Vladislav Goman <vlad.mangoman@gmail.com>
2021-11-03 02:58:27 +0100

    Added build folder to gitignore

--- dist/_next/static/PL99-OOIurtntt1Y4FvSb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/826-1e5d67ee9cf958eb.js","static/css/938f7fb7ed2dce1a.css","static/chunks/pages/index-f0cb524919d96f15.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
