6681082e9f9669770c6f157cabe83a4bdf1e40db
Vladislav Goman <vlad.mangoman@gmail.com>
2022-01-11 15:48:43 +0100

    AWS SNS update

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,26 +1,26 @@
-import TelegramBot from 'node-telegram-bot-api';
+import { SNSClient, PublishCommand } from '@aws-sdk/client-sns';
+
+const snsClient = new SNSClient({ region: process.env.AWS_REGION });
 
 const emailHandler = async (req, res) => {
   if (req.method === 'POST') {
     const { name, tel } = req.body;
 
-    const message = `Somebody has just submitted contact form on www.paradiseeventsplanner.com.
-    Contact details: 
-    Name: ${name} 
-    Telephone number: ${tel}`;
-
-    const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
-      polling: true,
-    });
+    const params = {
+      Message: `Hello, dear administrator!
+  Somebody has just submitted contact form on www.paradiseeventsplanner.com.
+  Contact details:
+  Name: ${name}
+  Telephone number: ${tel}
+  
+  Kindest regards,
+  paradiseeventsplanner web site`,
+      TopicArn: process.env.AWS_SNS_ARN, // TOPIC_ARN
+    };
 
     try {
-      const dataArray = [];
-      const chatIdArray = JSON.parse(process.env.TELEGRAM_CHAT_ID_ARRAY);
-      chatIdArray.forEach(async (chatId) => {
-        const data = await bot.sendMessage(chatId, message);
-        dataArray.push(data);
-      });
-      console.log('Success.', dataArray);
+      const data = await snsClient.send(new PublishCommand(params));
+      console.log('Success.', data);
     } catch (err) {
       console.log('Error', err.stack);
     }
