feaaaba0e316eacc9e5dfe5fc37d44331a7dd752
Vladislav Goman <vlad.mangoman@gmail.com>
2022-01-11 15:03:43 +0100

    Telegram notification added.

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,31 +1,29 @@
-import nodemailer from 'nodemailer';
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-});
+import TelegramBot from 'node-telegram-bot-api';
 
 const emailHandler = async (req, res) => {
   if (req.method === 'POST') {
     const { name, tel } = req.body;
 
-    let info = await transporter.sendMail({
-      from: '"ParadiseEventsPlanner"', // sender address
-      to: process.env.TARGET_EMAIL, // list of receivers
-      subject: 'Contact form filled on paradiseeventsplanner.com', // Subject line
-      html: `<h3>Hello, dear administrator!</h3> 
-      <p>Somebody has just submitted contact form on <a href="www.paradiseeventsplanner.com">www.paradiseeventsplanner.com</a>.</p>
-      <p> Contact details: </p>
-      <p>Name: ${name} </p>
-      <p>Telephone number: ${tel} </p>
-      <p>Kindest regards, </p>
-      <p> paradiseeventsplanner web site </p>`, // html body
+    const message = `Somebody has just submitted contact form on www.paradiseeventsplanner.com.
+    Contact details: 
+    Name: ${name} 
+    Telephone number: ${tel}`;
+
+    const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {
+      polling: true,
     });
 
-    console.log(info);
+    try {
+      const dataArray = [];
+      const chatIdArray = JSON.parse(process.env.TELEGRAM_CHAT_ID_ARRAY);
+      chatIdArray.forEach(async (chatId) => {
+        const data = await bot.sendMessage(chatId, message);
+        dataArray.push(data);
+      });
+      console.log('Success.', dataArray);
+    } catch (err) {
+      console.log('Error', err.stack);
+    }
 
     res.status(200);
     return res.end();
